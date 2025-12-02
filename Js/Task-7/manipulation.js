23f4fb5029030992fae290cf1140b32741805e07
Samiha Chowdhury <samiha.chowdhury99@gmail.com>
2025-12-03 04:33:41 +0600

    Added incomplete Js-7

---
+++ Js/Task-7/manipulation.js
@@ -0,0 +1,12 @@
+const para = document.getElementById("para");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+
+btn1.addEventListener("click", buttonOne);
+btn3.addEventListener("click", buttonTwo);
+btn3.addEventListener("click", buttonThree);
+
+function buttonOne(){};
+function buttonTwo(){};
+function buttonThree(){};
