536c55e010839172c7c4bc8dcbbef9f76a6e248c
Samiha Chowdhury <samiha.chowdhury99@gmail.com>
2025-12-04 21:46:27 +0600

    Js task-9 added

--- Js/Task-9/interaction.js
+++ Js/Task-9/interaction.js
@@ -37,10 +37,10 @@ function updateBtn(){
     }
 
     
-    if (current === "dark"){
+    else {
         button.textContent = "Switch to light mode";
         button.style.backgroundColor = "midnightblue";
-        button.style.color = "white";
+        button.style.color = "oldlace";
     }
 }
 
@@ -57,4 +57,89 @@ else if(now>=13 && now<=18){
 }
 else{
     greeting.innerHTML = "Good Evening from Samiha Chowdhury";
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event){
+    
+
+    let hasErr = false;
+
+    
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+
+    const nameErr = document.getElementById("nameErr");
+    const emailErr = document.getElementById("emailErr");
+    const messageErr = document.getElementById("messageErr");
+
+    const emailRegex=/\S+@\S+\.\S+/;
+
+
+    if(!emailRegex.test(email.value))
+        {
+            hasErr=true;
+            emailErr.innerHTML="Email pattern did not match";
+        }
+    if(message.value.length<10){
+        hasErr = true;
+        messageErr.innerHTML = "Message cannot be less than 10 characters";
+    }
+    if(name.value.trim()==""){
+        hasErr = true;
+        nameErr.innerHTML = "Name field cannot be empty";
+    }
+    
+    if(hasErr==true){
+        
+        event.preventDefault();
+            name.value = "";
+            email.value = "";
+            message.value = "";
+
+        
+    }
+    else{
+        alert("Message has been sent successfully!")
+            name.value = "";
+            email.value = "";
+            message.value = "";
+    }
+   
+})
+
+const about = document.getElementById("about");
+const project = document.getElementById("project");
+const contact = document.getElementById("contact");
+
+const aboutLink = document.getElementById("aboutLink");
+const projectLink = document.getElementById("projectLink");
+const contactLink = document.getElementById("contactLink");
+
+
+aboutLink.addEventListener("click", showAbout);
+projectLink.addEventListener("click", showProject);
+contactLink.addEventListener("click", showContact);
+
+function showAbout(){
+    about.style.display = "";
+    project.style.display = "none";
+    contact.style.display = "none";
+
+}
+
+function showProject(){
+    about.style.display = "none";
+    project.style.display = "";
+    contact.style.display = "none";
+
+}
+
+function showContact(){
+    about.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "";
+
 }
